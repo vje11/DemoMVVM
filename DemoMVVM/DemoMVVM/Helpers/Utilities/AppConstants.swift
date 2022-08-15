bffb191f9a588278cb430efdaac7fb9fe7dae115
Vijay Yadav <vijay.yadav@freecharge.com>
2022-08-15 20:00:13 +0530

    Helper classes added

---
+++ DemoMVVM/DemoMVVM/Helpers/Utilities/AppConstants.swift
@@ -0,0 +1,19 @@
+//
+//  APPConstants.swift
+//  DemoProject
+//
+//  Created by Vijay Yadav on 13/08/22.
+//  Copyright © 2021 Vijay Yadav. All rights reserved.
+//
+
+import Foundation
+
+
+struct AppConstants {
+    
+    static let landingTitle = "Landing_Title".localized
+}
+
+struct Dimensions {
+    static let cornerRadius = 8.0
+}
