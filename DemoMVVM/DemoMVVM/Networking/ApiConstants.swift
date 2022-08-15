38450c8107bccbfd00abf0ecbd905ea0350bc358
Vijay Yadav <vijay.yadav@freecharge.com>
2022-08-15 21:19:13 +0530

    Constant file for env variables

---
+++ DemoMVVM/DemoMVVM/Networking/ApiConstants.swift
@@ -0,0 +1,15 @@
+//
+//  ApiConstants.swift
+//  DemoMVVM
+//
+//  Created by Vijay Yadav on 15/08/22.
+//
+
+import Foundation
+
+/// Comment: The best way we can manage environment constants using xcconfig. below are the ways to just depict how to store api constant in a seperate file that is easily maintainable and configurable.
+
+struct ApiConstant {
+    static let token = "token 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"
+    static let baseUrl = "https://api.github.com/repos/vje11/DemoMVVM"
+}
